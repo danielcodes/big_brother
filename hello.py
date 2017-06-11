fbb87af4652695d5e954251ead4073cbe141cdf3
Daniel Chia <danielarmandochia@gmail.com>
2017-06-10 20:35:48 -0400

    created index page with table

---
+++ hello.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    # grab data
+    data = [
+            {'timestamp' : '06-10-17', 'ip_address' : '138.34.42.10', 'source_url' : 'google.com', 'current_url' : 'violence.com', 'lattitude' : '14', 'longitude' : '139'},
+            {'timestamp' : '06-10-17', 'ip_address' : '138.34.42.10', 'source_url' : 'google.com', 'current_url' : 'violence.com', 'lattitude' : '14', 'longitude' : '139'},
+            {'timestamp' : '06-10-17', 'ip_address' : '138.34.42.10', 'source_url' : 'google.com', 'current_url' : 'violence.com', 'lattitude' : '14', 'longitude' : '139'},
+            {'timestamp' : '06-10-17', 'ip_address' : '138.34.42.10', 'source_url' : 'google.com', 'current_url' : 'violence.com', 'lattitude' : '14', 'longitude' : '139'},
+           ]
+    
+    # pass data to template
+
+
+    return render_template('index.html', data=data)
+
+if __name__ == "__main__":
+    app.run(debug=True)
