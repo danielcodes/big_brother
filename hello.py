cc0fe6c4ffb524664d685579de34b066f4d1b577
Daniel Chia <danielarmandochia@gmail.com>
2017-06-11 09:53:29 -0400

    data is read from csv file

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import csv
 app = Flask(__name__)
 
 @app.route("/")
@@ -8,17 +9,27 @@ def home():
     # pull in data from csv file
 
     # grab data
-    data = [
-            {'timestamp' : '06-10-17', 'ip_address' : '138.34.42.10', 'source_url' : 'google.com', 'current_url' : 'violence.com', 'lattitude' : '14', 'longitude' : '139'},
-            {'timestamp' : '06-10-17', 'ip_address' : '138.34.42.10', 'source_url' : 'google.com', 'current_url' : 'violence.com', 'lattitude' : '14', 'longitude' : '139'},
-            {'timestamp' : '06-10-17', 'ip_address' : '138.34.42.10', 'source_url' : 'google.com', 'current_url' : 'violence.com', 'lattitude' : '14', 'longitude' : '139'},
-            {'timestamp' : '06-10-17', 'ip_address' : '138.34.42.10', 'source_url' : 'google.com', 'current_url' : 'violence.com', 'lattitude' : '14', 'longitude' : '139'},
-           ]
+    # data = [
+            # {'timestamp' : '06-10-17', 'ip_address' : '138.34.42.10', 'source_url' : 'google.com', 'current_url' : 'violence.com', 'lattitude' : '14', 'longitude' : '139'},
+            # {'timestamp' : '06-10-17', 'ip_address' : '138.34.42.10', 'source_url' : 'google.com', 'current_url' : 'violence.com', 'lattitude' : '14', 'longitude' : '139'},
+            # {'timestamp' : '06-10-17', 'ip_address' : '138.34.42.10', 'source_url' : 'google.com', 'current_url' : 'violence.com', 'lattitude' : '14', 'longitude' : '139'},
+            # {'timestamp' : '06-10-17', 'ip_address' : '138.34.42.10', 'source_url' : 'google.com', 'current_url' : 'violence.com', 'lattitude' : '14', 'longitude' : '139'},
+           # ]
     
+    data = []
     # pass data to template
-
+    with open('data/janak_data.csv', 'rU') as f:
+        reader = csv.reader(f)
+        header = reader.next()
+        # print header
+        for row in reader:
+            formatted = dict(zip(header, row))
+            data.append(formatted)
+            # print formatted
 
     return render_template('index.html', data=data)
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
