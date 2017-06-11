b1f9de64bdcc46ecbdb27a4092a6e34cd85bcfd3
Daniel Chia <danielarmandochia@gmail.com>
2017-06-11 11:15:37 -0400

    changed response to for ip addresses to have list of activity data

--- hello.py
+++ hello.py
@@ -1,33 +1,42 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, json
 import csv
 app = Flask(__name__)
 
+# precompute values, want to group the rows with the same ip addresses and create a line chart
+data = []
+ip_addresses = {}
+
+# pass data to template
+with open('data/janak_data.csv', 'rU') as f:
+    reader = csv.reader(f)
+    header = reader.next()
+    # print header
+    
+    # data format should be, {ip: data_row}
+    # if seen for the first time, place it
+    # otherwise add the object to the list
+
+    for row in reader:
+        formatted = dict(zip(header, row))
+        if formatted['ip_address'] in ip_addresses:
+            ip_addresses[formatted['ip_address']].append(formatted)
+        else:
+            ip_addresses[formatted['ip_address']] = [formatted]
+
+        data.append(formatted)
+        # print formatted
+
+    print '\nPrinting ip address wit the grouped rows==============================================='
+    print ip_addresses
+
+
 @app.route("/")
 def home():
-    # 2 goals
-    # get the tooltip to display a title, image
-    # pull in data from csv file
+    return render_template('index.html', ip_addresses=ip_addresses)
 
-    # grab data
-    # data = [
-            # {'timestamp' : '06-10-17', 'ip_address' : '138.34.42.10', 'source_url' : 'google.com', 'current_url' : 'violence.com', 'lattitude' : '14', 'longitude' : '139'},
-            # {'timestamp' : '06-10-17', 'ip_address' : '138.34.42.10', 'source_url' : 'google.com', 'current_url' : 'violence.com', 'lattitude' : '14', 'longitude' : '139'},
-            # {'timestamp' : '06-10-17', 'ip_address' : '138.34.42.10', 'source_url' : 'google.com', 'current_url' : 'violence.com', 'lattitude' : '14', 'longitude' : '139'},
-            # {'timestamp' : '06-10-17', 'ip_address' : '138.34.42.10', 'source_url' : 'google.com', 'current_url' : 'violence.com', 'lattitude' : '14', 'longitude' : '139'},
-           # ]
-    
-    data = []
-    # pass data to template
-    with open('data/janak_data.csv', 'rU') as f:
-        reader = csv.reader(f)
-        header = reader.next()
-        # print header
-        for row in reader:
-            formatted = dict(zip(header, row))
-            data.append(formatted)
-            # print formatted
-
-    return render_template('index.html', data=data)
+@app.route("/get_data")
+def get_data():
+    return json.jsonify(ip_addresses)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/js/main.js
+++ static/js/main.js
@@ -1,9 +1,16 @@
 
 $(document).ready(function() {
   $('.tooltiper').tooltipster({
-    content: $('#tooltip_content') 
+    content: $('#tooltip_content'),
+    contentCloning: true
   });
+
+  $('#test').on('click', function(){
+    $.get('/get_data')
+      .success(function(data){
+        console.log(data) 
+      })
+  });
+
 });
  
-
-
